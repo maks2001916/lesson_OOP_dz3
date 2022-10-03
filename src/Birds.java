8df8be54468b835c6d528a57c5bd6c4dc19b1289
maks <mamonov.maksim.20003@gmail.com>
2022-10-03 12:12:52 +0500

    класс birds заполнен

--- src/Birds.java
+++ src/Birds.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Birds extends Animals{
     private String livingEnvironment;  //среда обитания
 
@@ -8,5 +10,41 @@ public class Birds extends Animals{
 
     public void hunt() {  //охотиться
         System.out.println("охотится днём или ночью");
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void moveAround() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Birds birds = (Birds) o;
+        return Objects.equals(livingEnvironment, birds.livingEnvironment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), livingEnvironment);
+    }
+
+    @Override
+    public String toString() {
+        return "птица{" +
+                "среда обитания - '" + livingEnvironment + '\'' +
+                '}';
     }
 }
